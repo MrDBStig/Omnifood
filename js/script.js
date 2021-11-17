1ead643a41e67797f0c3b0d5e915c84c12672d43
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-11-17 22:34:20 +0300

    Scripted mobile navigation

--- js/script.js
+++ js/script.js
@@ -18,9 +18,20 @@ function checkFlexGap() {
 }
 checkFlexGap();
 
+///////////////////////////////////////////////////////////
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Mobile navigation script
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
