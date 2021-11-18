5497f71ac80b49cba88b2998042ce79def26f72d
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-11-18 09:48:26 +0300

    Completed smooth scrolling in all browsers

--- js/script.js
+++ js/script.js
@@ -33,7 +33,36 @@ btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
 
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+///////////////////////////////////////////////////////////
+// Smooth scroling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scrol to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js (for Safari)
 
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
